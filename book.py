f353728385980a433cf434cfa07df0b164b60907
Henri Winckel <henri.winckel@edu.devinci.fr>
2022-03-31 17:28:11 +0200

    buy and sell inserting implementation in Book

--- book.py
+++ book.py
@@ -1,8 +1,16 @@
 class Book:
-    def Book(self):
-        return
+    def __init__(self, name):
+        self.name = name
+        self.buy_list = []
+        self.sell_list = []
+    
+    def insert_buy(self, qty, price):
+        self.buy_list.append([qty, price])
+
+    def insert_sell(self, qty, price):
+        self.sell_list.append([qty, price])
     
 
 class Order:
-    def Order(self):
+    def __init__(self):
         return
