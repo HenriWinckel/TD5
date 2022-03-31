82190573df353952ed0125ef470f7a81eca15363
Henri Winckel <henri.winckel@edu.devinci.fr>
2022-03-31 18:00:44 +0200

    print insert buy/sell

--- book.py
+++ book.py
@@ -3,14 +3,24 @@ class Book:
         self.name = name
         self.buy_list = []
         self.sell_list = []
+        self.count = 0
     
     def insert_buy(self, qty, price):
-        self.buy_list.append([qty, price])
+        self.buy_list.append(Order(qty, price, "buy", self.count+1))
+        self.count += 1
+
+        print(f"--- Insert BUY {qty}@{price} id={self.count} on {self.name}")
 
     def insert_sell(self, qty, price):
-        self.sell_list.append([qty, price])
-    
+        self.sell_list.append(Order(qty, price, "sell", self.count+1))
+        self.count += 1
+        print(f"--- Insert SELL {qty}@{price} id={self.count} on {self.name}")
+
 
 class Order:
-    def __init__(self):
+    def __init__(self, qty, price, type, id):
+        self.qty = qty
+        self.price = price
+        self.type = type
+        self.id = id
         return
